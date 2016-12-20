64647fd6e4484dedc7b36d6e97a21a87814a4bbe
momotaro <su.mo.mo.mo.momotaro@gmail.com>
2016-12-20 10:45:28 +0100

    start 4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup 
-	  @title = "Ore no page"
+	  @title = "Ore no page !!"
   end
 
   test "should get root" do
@@ -14,7 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@title}" 
+    assert_select "title", "#{@title}" 
   end
 
   test "should get help" do
