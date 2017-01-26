f6a945aa29e9afbfd8d5d63b453a49e87bace56a
miyzawa <b1504527@planet.kanazawa-it.ac.jp>
2017-01-26 13:09:48 +0100

    relation succes

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170126114227) do
+ActiveRecord::Schema.define(version: 20170126115257) do
 
   create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
     t.text     "content",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 20170126114227) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tweets", "users"
 end
